613f68e145ff59fdfc30b690127bfc00d39d27a4
Lucas Diaz <lucasdm0905@gmail.com>
2024-05-26 11:34:40 -0300

    commit

---
+++ Java Básico/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        //array se inicia em 0
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é : " + aluno);
+        }
+    }
+    
+}
